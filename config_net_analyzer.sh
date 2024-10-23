0804269bbe31bc212b4336ade9e33cd5304c28c5
Zephyr <MAX06vincent@gmail.com>
2024-10-23 10:54:40 +0200

    Update config_net_analyzer.sh

--- config_net_analyzer.sh
+++ config_net_analyzer.sh
@@ -18,6 +18,16 @@ disable_promiscuous_mode() {
        fi
    done
 }
+check_lib_available(){
+    if dpkg -l | grep -qw "$1"; then
+        echo "$1 is already installed."
+    else
+        echo "$1 not installed. Launch installation..."
+        apt-get install $1
+    fi
+}
+# Set up Wifi for possible library to install
+sudo ip link set wlan0 up
 # Delete every existing bridge and dissociate the interfaces
 echo "# Deletion of every existing bridges..."
 for bridge in $(brctl show | awk 'NR>1 {print $1}' | sort -u); do
@@ -49,6 +59,12 @@ if ! grep -q "net.ipv4.ip_forward=1" /etc/sysctl.conf; then
 else
    echo "# Forwarding IP is already configured in /etc/sysctl.conf."
 fi
+# Uninstall nftables and its dependencies
+sudo apt-get remove --auto-remove nftables
+apt-get purge nftables
+#Install iptables
+echo "# Verification and installation of iptables if needed..."
+check_lib_available iptables
 # Empty the iptables existing rules to start from scratch
 echo "# Reinitialisation of iptables rules..."
 iptables -F
@@ -59,10 +75,14 @@ iptables -A FORWARD -i eth1 -o eth2 -j ACCEPT
 iptables -A FORWARD -i eth2 -o eth1 -j ACCEPT
 # Install bridge-utils if needed (useful for the bridge management)
 echo "# Verification and installation of bridge-utils if needed..."
-apt-get install -y bridge-utils
+check_lib_available bridge-utils
 # Create a new bridge br0
 echo "# Create a new bridge br0..."
 brctl addbr br0
+# Add top priority to the bridge br0
+brctl setbridgeprio br0 0
+# Set lowest bridge forwarding time delay
+brctl setfd br0 0
 # Add eth1 and eth2 on bridge (the eth0 interface will be used for remote capture)
 echo "# Add the interfaces eth1 and eth2 on bridge br0..."
 brctl addif br0 eth1
@@ -86,3 +106,6 @@ echo "#  - Deletion of the older existing bridge."
 echo "#  - Forwarding activation on eth1 and eth2."
 echo "#  - The interface eth0 is configured in promiscuity mode to capture the network traffic."
 echo "#  - You can now use Wireshark on eth0 to sniff all the network traffic between eth1 and eth2."
+
+# Stop and disable Wifi to avoid some external process
+sudo ip link set wlan0 down 
