118a10d203fdecc1f7ed803d7c5e5ebc7396dedb
Zephyr <MAX06vincent@gmail.com>
2024-11-13 18:52:39 +0100

    Update eth_iface_boot.sh

--- eth_iface_boot.sh
+++ eth_iface_boot.sh
@@ -7,7 +7,7 @@ SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service"
 
 # Check if the target script exists
 if [[ ! -f "$SCRIPT_PATH" ]]; then
-  echo "# Error: Script $SCRIPT_PATH does not exist."
+  echo "Error: Script $SCRIPT_PATH does not exist."
   exit 1
 fi
 
@@ -27,7 +27,7 @@ RemainAfterExit=true
 WantedBy=multi-user.target" | sudo tee "$SERVICE_FILE" > /dev/null
 
 # Set the correct permissions for the service file
-sudo chmod 666 "$SERVICE_FILE"
+sudo chmod 777 "$SERVICE_FILE"
 
 # Reload systemd to apply changes
 sudo systemctl daemon-reload
@@ -35,8 +35,7 @@ sudo systemctl daemon-reload
 # Enable the service to start on boot
 sudo systemctl enable "$SERVICE_NAME.service"
 
-echo "# Service $SERVICE_NAME has been created and enabled to start at boot."
+echo "Service $SERVICE_NAME has been created and enabled to start at boot."
 
 sudo systemctl start "$SERVICE_NAME.service"
-echo "# Service $SERVICE_NAME started."
-echo "----------------------------------------------------"
+echo "Service $SERVICE_NAME started."
