cd382330a0ca7f3d1156334d790c8f6bf2becb6b
Zephyr <MAX06vincent@gmail.com>
2024-11-13 16:06:26 +0100

    Create requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+sudo apt-get install tshark
+sudo apt-get install iptables
+sudo apt-get install bridge-utils
+sudo apt-get install nodejs
+sudo apt-get install npm
+sudo npm install dpkg
+sudo apt-get install libpam0g-dev
