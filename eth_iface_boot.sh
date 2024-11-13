4638ddd6324d0538059612f81c6026460e279c61
Zephyr <MAX06vincent@gmail.com>
2024-11-13 17:59:50 +0100

    Update eth_iface_boot.sh

--- eth_iface_boot.sh
+++ eth_iface_boot.sh
@@ -7,7 +7,7 @@ SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service"
 
 # Check if the target script exists
 if [[ ! -f "$SCRIPT_PATH" ]]; then
-  echo "Error: Script $SCRIPT_PATH does not exist."
+  echo "# Error: Script $SCRIPT_PATH does not exist."
   exit 1
 fi
 
@@ -35,7 +35,8 @@ sudo systemctl daemon-reload
 # Enable the service to start on boot
 sudo systemctl enable "$SERVICE_NAME.service"
 
-echo "Service $SERVICE_NAME has been created and enabled to start at boot."
+echo "# Service $SERVICE_NAME has been created and enabled to start at boot."
 
 sudo systemctl start "$SERVICE_NAME.service"
-echo "Service $SERVICE_NAME started."
+echo "# Service $SERVICE_NAME started."
+echo "----------------------------------------------------"
