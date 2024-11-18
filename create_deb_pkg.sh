385ffbaad516d365abefff083e22ff2de544164f
Zephyr <MAX06vincent@gmail.com>
2024-11-18 14:53:53 +0100

    modify requirements and update script for package creation

--- create_deb_pkg.sh
+++ create_deb_pkg.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-sudo mkdir libs
-cd libs
+sudo mkdir libs && sudo chmod 777 libs
 
+
+echo "[INFO] Generate lib dependencies"
 while read -r package; do
-   apt-rdepends "$package" | grep -v "^ " >> full-package-list.txt
+   apt-rdepends "$package" | grep -v "^ " >> libs/full-package-list.txt
 done < package-list.txt
+
+cd libs
 
 sort -u full-package-list.txt -o full-package-list.txt
 
 while read -r package; do
+   echo "[INFO] Download package: $package"
    apt-get download "$package" -y
 done < full-package-list.txt
 
-cd ..
-for deb in libs/*.deb; do
-   dpkg-deb -x "$deb" /home/sPIffer
-done
-
-sudo rm -rf libs
-cd ..
-sudo chmod 777 -R sPIffer
+cd ../..
+sudo chmod 775 -R sPIffer
+sudo chown -R root:root sPIffer
 sudo dpkg-deb --build sPIffer
 sudo mkdir sPIffer/dist
-sudo mv *.deb /sPIffer/dist
+sudo mv *.deb sPIffer/dist
--- requirements.sh
+++ requirements.sh
@@ -7,6 +7,4 @@ sudo apt-get install iptables
 sudo apt-get install bridge-utils
 sudo apt-get install nodejs
 sudo apt-get install npm
-sudo npm install dpkg
 sudo apt-get install libpam0g-dev
-sudo apt-get install debhelper
