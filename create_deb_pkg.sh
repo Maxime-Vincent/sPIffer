02a577952072e582117b96321e1685dccfee7b6a
Zephyr <MAX06vincent@gmail.com>
2024-11-20 11:44:43 +0100

    Update package installation
    Remove useless logs
    Update README removing useless package dependencies
    Remove preparation for debian library needed > not ready and debian package is too big

--- create_deb_pkg.sh
+++ create_deb_pkg.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 
-sudo mkdir libs && sudo chmod 777 libs
-
-
-echo "[INFO] Generate lib dependencies"
-while read -r package; do
-   apt-rdepends "$package" | grep -v "^ " >> libs/full-package-list.txt
-done < package-list.txt
-
-cd libs
-
-sort -u full-package-list.txt -o full-package-list.txt
-
-while read -r package; do
-   echo "[INFO] Download package: $package"
-   apt-get download "$package" -y
-done < full-package-list.txt
-
-cd ../..
+cd ..
 sudo chmod 775 -R sPIffer
 sudo chown -R root:root sPIffer
 sudo dpkg-deb --build sPIffer
--- home/sPIffer/eth_iface_boot.sh
+++ home/sPIffer/eth_iface_boot.sh
@@ -7,11 +7,12 @@ SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service"
 
 # Check if the target script exists
 if [[ ! -f "$SCRIPT_PATH" ]]; then
-  echo "Error: Script $SCRIPT_PATH does not exist."
+  echo "# Error: Script $SCRIPT_PATH does not exist."
   exit 1
 fi
 
 # Create the systemd service file
+echo "# Create the systemd service file"
 echo "[Unit]
 Description=sPIffer conf at boot
 Wants=network-pre.target
@@ -27,15 +28,14 @@ RemainAfterExit=true
 WantedBy=multi-user.target" | sudo tee "$SERVICE_FILE" > /dev/null
 
 # Set the correct permissions for the service file
+echo "# Set the correct permissions for the service file"
 sudo chmod 777 "$SERVICE_FILE"
 
 # Reload systemd to apply changes
+echo "# Reload systemd to apply changes"
 sudo systemctl daemon-reload
 
 # Enable the service to start on boot
 sudo systemctl enable "$SERVICE_NAME.service"
 
-echo "Service $SERVICE_NAME has been created and enabled to start at boot."
-
-sudo systemctl start "$SERVICE_NAME.service"
-echo "Service $SERVICE_NAME started."
+echo "# Service $SERVICE_NAME has been created and enabled to start at boot."
