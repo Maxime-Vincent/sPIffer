084a28f0372283cb024280f3304a3fbc359253bb
Zephyr <MAX06vincent@gmail.com>
2024-12-09 15:48:28 +0100

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -3,9 +3,9 @@
 set -e
 
 sudo apt-get update
-sudo apt-get install tshark
-sudo apt-get install iptables
-sudo apt-get install bridge-utils
-sudo apt-get install nodejs
-sudo apt-get install npm
-sudo apt-get install libpam0g-dev
+sudo apt-get install tshark==4.0.17
+sudo apt-get install iptables==1.8.9
+sudo apt-get install bridge-utils==1.7.1
+sudo apt-get install nodejs==18.19.0
+sudo apt-get install npm==9.2.0
+sudo apt-get install libpam0g-dev==1.5.2
