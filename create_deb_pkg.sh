39b4b6538957de0527b74634b6c28f432ae93e13
Zephyr <MAX06vincent@gmail.com>
2025-02-06 14:40:11 +0100

    Update create_deb_pkg.sh

--- create_deb_pkg.sh
+++ create_deb_pkg.sh
@@ -4,5 +4,6 @@ cd ..
 sudo chmod 775 -R sPIffer
 sudo chown -R root:root sPIffer
 sudo dpkg-deb --build sPIffer
-sudo mkdir sPIffer/dist
+sudo mkdir -p sPIffer/dist
+sudo rm -rf sPIffer/dist
 sudo mv *.deb sPIffer/dist
