8fdced8d5dc52b30a66ca620ffdc1f02735f5cbb
Zephyr <MAX06vincent@gmail.com>
2025-02-06 16:30:11 +0100

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -3,7 +3,7 @@
 set -e
 
 sudo apt-get update
-sudo apt-get install tshark=4.0.17
+sudo apt-get install tshark
 sudo apt-get install iptables=1.8.9
 sudo apt-get install bridge-utils=1.7.1
 sudo apt-get install nodejs=18.19.0
